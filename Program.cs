c0727179a3c41f772ec68981957c91023d1e6b76
SAMANDAR <dotnetdasturchi@gmail.com>
2023-06-07 17:04:34 +0500

    Birinchi commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode_Masala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            n.ToString();
+            Console.WriteLine(n);
+        }
+    }
+}
